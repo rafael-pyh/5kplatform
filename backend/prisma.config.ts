e45f6f17e8a76b900b8aa8705e3761618c44d3da
rafael-pyh <pyhbmx@gmail.com>
2025-12-01 12:59:15 -0300

    fix: schema prisma

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,13 +2,5 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
-
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-
-  // para o Prisma Client (node)
-  client: {
-    adapter: 'postgresql',
-  },
 });
+
