cbf2906d6744dcc97b80ec541a5932cb58d64c63
rafael-pyh <pyhbmx@gmail.com>
2025-12-02 15:03:09 -0300

    Add detailed Prisma Client generation logging

--- backend/prisma.config.js
+++ backend/prisma.config.js
@@ -2,8 +2,7 @@ module.exports = {
   schema: './prisma/schema.prisma',
   datasources: {
     db: {
-      url: process.env.DATABASE_URL || '',
+      url: process.env.DATABASE_URL || 'postgresql://user:pass@localhost:5432/db',
     },
   },
 };
-
