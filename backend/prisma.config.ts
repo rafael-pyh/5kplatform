51bcc8bdef8596f3b25845e95746cd29b446c141
rafael-pyh <pyhbmx@gmail.com>
2025-12-02 14:59:03 -0300

    Fix prisma.config.ts format for Prisma 7

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   schema: './prisma/schema.prisma',
   datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url: process.env.DATABASE_URL || '',
     },
   },
 };
