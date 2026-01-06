b3c87e5f8731151009e7fb80f7937361797be8f6
rafael-pyh <pyhbmx@gmail.com>
2026-01-06 17:47:33 -0300

    feat: change sidebar to header

--- frontend/5k-energia-solar/components/DashboardLayout.tsx
+++ frontend/5k-energia-solar/components/DashboardLayout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { ReactNode } from 'react';
-import Sidebar from '@/components/Sidebar';
+import Header from '@/components/Header';
 import { cn } from '@/lib/utils/cn';
 
 interface DashboardLayoutProps {
@@ -11,9 +11,9 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children, className }: DashboardLayoutProps) {
   return (
-    <div className={cn('flex min-h-screen bg-gray-50', className)}>
-      <Sidebar />
-      <main className="flex-1 ml-4 pl-64 p-8 w-full min-w-0 bg-linear-to-br from-blue-50 to-green-50">{children}</main>
+    <div className={cn('flex flex-col min-h-screen bg-gray-50', className)}>
+      <Header />
+      <main className="flex-1 p-4 sm:p-6 lg:p-8 w-full min-w-0 bg-linear-to-br from-blue-50 to-green-50">{children}</main>
     </div>
   );
 }
