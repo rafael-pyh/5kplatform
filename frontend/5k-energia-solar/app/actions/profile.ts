e321ccbc3e1ac9bf93a10637282fa11e27272525
rafael-pyh <pyhbmx@gmail.com>
2026-01-22 16:14:47 -0300

    fix: error in profile action

--- frontend/5k-energia-solar/app/actions/profile.ts
+++ frontend/5k-energia-solar/app/actions/profile.ts
@@ -1,6 +1,8 @@
+import { personService } from "@/lib/services";
+
 export const updateProfileAction = async (sellerId: string, formData: any) => {
   try {
-    const response = await api.put(`/seller/${sellerId}`, formData);
+    const response = await personService.update(sellerId, formData);
     return response;
   } catch (error: any) {
     console.error("Erro ao atualizar perfil:", error);
